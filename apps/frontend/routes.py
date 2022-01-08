42450cb55e8ffcd099b9ff8c60fe3d3158eb125a
ilya-goldin <rtrwv88@gmail.com>
2022-01-08 23:22:44 +0300

    fix: Remove aiohttp_middlewares lib

--- apps/frontend/routes.py
+++ apps/frontend/routes.py
@@ -1,21 +1,10 @@
-from re import compile
 from .views import Handler
-from aiohttp_middlewares import cors_middleware
-from aiohttp_middlewares.cors import DEFAULT_ALLOW_HEADERS
-from settings import CORS_ALLOW_ORIGINS
 
 
-def frontend_routes(app):
+def frontend_routes(application):
     """
     Append frontend routes and middleware
-    :param app:
-    :type app: aiohttp.web.Application
+    :param application:
+    :type application: aiohttp.web.Application
     """
-    app.router.add_get("/", Handler)
-    app.middlewares.append(
-        cors_middleware(
-            origins=[compile(r"^https?\:\/\/localhost")] + CORS_ALLOW_ORIGINS,
-            allow_methods=["GET"],
-            allow_headers=DEFAULT_ALLOW_HEADERS,
-        )
-    )
+    application.router.add_get("/", Handler)

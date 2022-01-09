bf4bbafb5a1cc07f9ddd807757894670cf866077
ilya-goldin <rtrwv88@gmail.com>
2022-01-09 13:05:01 +0300

    fix: Remove middleware.py

--- apps/api/middleware.py
+++
@@ -1,9 +0,0 @@
-from aiohttp.web import Request, Response, json_response
-from aiohttp_middlewares import error_context
-
-
-async def api_error(request: Request) -> Response:
-    with error_context(request) as context:
-        return json_response(
-            context.data, status=context.status
-        )

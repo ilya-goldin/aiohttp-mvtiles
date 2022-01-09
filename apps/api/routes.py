3849c913c8306c23ca60051fcd1014f2bdc3e136
ilya-goldin <rtrwv88@gmail.com>
2022-01-09 13:05:30 +0300

    doc: Add docstring

--- apps/api/routes.py
+++ apps/api/routes.py
@@ -3,9 +3,10 @@ from .views import MVTView
 
 def api_routes(application, cors):
     """
-    Append api routes and middleware
-    :param application:
-    :type application: aiohttp.web.Application
+    Add API routes to the application.
+
+    :param aiohttp_cors.CorsConfig cors: CORS configuration instance
+    :param aiohttp.web.Application application: Application
     """
     api1 = cors.add(application.router.add_resource("/api/v1/t.mvt"))
     api2 = cors.add(application.router.add_resource(r"/api/v1/tile/{z:\d+}/{x:\d+}/{y:\d+}"))

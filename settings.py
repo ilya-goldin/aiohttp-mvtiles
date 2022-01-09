1bff0d7dd91a8d017646652717cf53b5249e85e1
ilya-goldin <rtrwv88@gmail.com>
2022-01-09 13:12:47 +0300

    feat: Add general application settings

--- settings.py
+++ settings.py
@@ -1,10 +1,18 @@
 from os import path, getenv
 from dotenv import load_dotenv
+from db.models import ExampleLayer
 
 load_dotenv()
 
 DATABASE_URL = getenv("DATABASE_URL")
 POSTGIS_3 = True
+
+LAYERS = [ExampleLayer]
+LAYERS_SRID = 4326
+EXTEND = 4096
+BUFFER = 256
+CLIP = False
+
 
 TEMPLATES_DIR = path.join("apps", "frontend", "templates")
 
